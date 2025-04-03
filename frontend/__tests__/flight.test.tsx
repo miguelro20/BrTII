3a6c6f24854ddf7e7cc72e095b87c606e2af4fec
miguel.ruiz <miguel.ruiz@encora.com>
2025-04-03 06:03:26 -0600

    Modularization of flight page

--- frontend/__tests__/flight.test.tsx
+++ frontend/__tests__/flight.test.tsx
@@ -1,13 +1,13 @@
 import {beforeEach, describe, expect, test, vi} from 'vitest'
 import {render, screen} from "@testing-library/react"
-import FlightsPage from '@/app/flights/page'
+import FlightsDetails from '@/components/flights/flights-details'
 
 describe("FlightPage", () => {
     beforeEach(()=> {
         vi.clearAllMocks()
     })
 
-    render(<FlightsPage/>)
+    render(<FlightsDetails/>)
     test("renders Found", () => {
         expect(screen.getByText(/Found/i)).toBeDefined()
     })
